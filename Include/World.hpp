1f458684f690551757060307d078118721437b88
gerkone <gianlucagalletti@ymail.com>
2019-04-03 14:57:23 +0200

    added terrain - removed old src

--- Include/World.hpp
+++ Include/World.hpp
@@ -2,14 +2,41 @@
 #define WORLD_HPP
 
 #include <SFML/Graphics.hpp>
+
+#include <list>
+#include <time.h>
+#include <iostream>
+#include <math.h>
+
+const int SEGMENT_LIMIT = 1000; //segmenti massimi che comporranno il terreno
+const int MAX_VIEWS = 5; //schermate massime
+const int MIN_VIEWS = 3; //schermate massime
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 500;
+
+const double VARIANCE = 0.55; //ruvidità di scalo tra displacement tra passaggi successivi
+const double DISPLACEMENT = 400; // unità di spostamento (segs * displacement) in px rispetto al numero di segmenti
+
+struct voxel {
+  float x;
+  float y;
+};
+
 class World{
   public:
     //spawn coordinate of the circle in the main window
     World(float x, float y);
     sf::CircleShape getCircle();
-
+    sf::Vector2f getTerrain(int v);
+    void nextView();
+    void prevView();
   private:
     sf::CircleShape mPlanet;
-
+    sf::Vector2f vx*;
+    int seeds*;
+    int views;
+    int itV;  //iteratore per la view corrente
+    void terrainGen(sf::Vector2f vx[], int start, int end, double displ);
 };
 #endif //WORLD_HPP
--- Source/World.cpp
+++ Source/World.cpp
@@ -5,7 +5,53 @@ World::World(float x, float y){
   mPlanet.setOutlineColor(sf::Color::Red);
   mPlanet.setOutlineThickness(5);
   mPlanet.setPosition(x,y);
+
+  srand(time(0));
+  views = rand()%(MAX_VIEWS - MIN_VIEWS) + MIN_VIEWS;
+  vx = new sf::Vector2f[SEGMENT_LIMIT];
+  seeds = new int[views];
+  for(int c = 0; c < views; c++)
+  {
+      seeds[c] = rand()%1337;
+  }
+  for(int c = 0; c < SEGMENT_LIMIT; c++) {
+    vx[c] = ((float) ((SCREEN_WIDTH/SEGMENT_LIMIT) * c), (float) 0);
+  }
 }
+
+void nextView() {
+  itV = (itV + 1) % views;
+}
+
+
+void prevView() {
+  itV = (itV - 1) % views;
+}
+
+sf::Vector2f* getTerrain() {
+  // try {
+    srand(seeds[itV]);
+    terrainGen();
+    return vx;
+  // } catch {
+  //
+  // }
+}
+
+void terrainGen(int start = 0, int end = SEGMENT_LIMIT, double displ = DISPLACEMENT) {
+  if(start + 1 >= end) {}
+  else
+  {
+      int mid = (end + start) / 2;
+      double rnd = ((double) rand()/(RAND_MAX)) - 0.5;
+      float offset = displ * rnd;
+      vx[mid].y = (vx[start].y + vx[end].y)/2 + offset;
+      displ = VARIANCE * displ; // displacement scalato del fattore variance
+      terrainGen(vx, start, mid, displ); // reiterazione sui lati sx e dx del segmento
+      terrainGen(vx, mid, end, displ);
+  }
+}
+
 
 sf::CircleShape World::getCircle(){
   return mPlanet;
